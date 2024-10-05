a5cae2c7ca2ee4bf5191541376895b5d8f059ef7
Dovias <70913423+Dovias@users.noreply.github.com>
2024-10-05 19:20:33 +0300

    add oo and OO remaps to add newlines in nvim NORMAL mode

--- nvim/lua/config/remaps.lua
+++ nvim/lua/config/remaps.lua
@@ -1,6 +1,9 @@
 -- Diagnostics are not exclusive to lsp servers
 -- so these can be global keybindings
-vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
-vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
-vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>') 
+vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>")
+vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
+vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>") 
 
+-- Add oo and OO keymaps to add newlines in NORMAL mode:
+vim.keymap.set("n", "oo", "m`o<Esc>``")
+vim.keymap.set("n", "OO", "m`O<Esc>``")
